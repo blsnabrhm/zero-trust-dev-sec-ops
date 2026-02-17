38962ea77f989891ca75dd5cc36b84df8697a077
Blesson Biju Abraham <bbabrahambb@gmail.com>
2026-02-18 04:23:01 +0530

    initial commit with vulnerablity

---
+++ src/main/java/com/example/demo/ZtdsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtdsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtdsoApplication.class, args);
+	}
+
+}
